f810021eda427fb873e9c34f6c2e2fc75f0cd6e0
cnobre <carolinanobre@gmail.com>
2017-05-03 16:21:34 -0600

    added files to gitignore

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import { mixin } from 'phovea_core/src';
-export function hello(options) {
-    //merge with default options
-    options = mixin({
-        name: 'World'
-    }, options);
-    return "Hello " + options.name;
-}
-//# sourceMappingURL=index.js.map
